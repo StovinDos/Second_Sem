bad37b35d57f92f177ff70459899b97c4177e555
Stoyan Georgiev <stovingg@gmail.com>
2025-02-25 21:29:50 +0000

    Feb 25, 2025, 11:29 PM

--- Week_1_dom/zad_2.cpp
+++ Week_1_dom/zad_2.cpp
@@ -19,16 +19,16 @@ void calculateResult(int x, int y, int z){
         cout<< "There are no real roots" << endl;
     
     } else if (d == 0){
-        cout << "There is one real root" << endl;
+        cout << "There is one real root (x1 = x2)" << endl;
         result = -y / (2*x);
         cout << "The root is: " << result << endl;
 
     } else{
         cout << "There are two real roots" << endl;
         result = (-y + sqrt(d)) / (2*x);
-        cout << "The first root is: " << result << endl;
+        cout << "The first root is (x1): " << result << endl;
         result = (-y - sqrt(d)) / (2*x);
-        cout << "The second root is: " << result << endl;
+        cout << "The second root is (x2): " << result << endl;
     }
 }
 
