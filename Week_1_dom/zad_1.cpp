89a0ff8212e889043a5e98ab162c0eac597a3821
Stoyan Georgiev <stovingg@gmail.com>
2025-02-25 20:20:23 +0000

    Feb 25, 2025, 10:20 PM

---
+++ Week_1_dom/zad_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int N;
+
+int x, y, z;
+
+// enter the number of points
+
+cin >> N;
+
+cin >> x >> y >> z;
+cout << x << " " << y << " " << z << endl;
+
+cin >> x >> y >> z;
+cout << x << " " << y << " " << z << endl;
+
+
+}
