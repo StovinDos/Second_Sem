83f2b85c16197084eca6534d78b323913238ef91
Stoyan Georgiev <117667767+StovinDos@users.noreply.github.com>
2025-02-26 19:04:23 +0200

    Update zad_3.cpp

--- Week_1_dom/zad_3.cpp
+++ Week_1_dom/zad_3.cpp
@@ -6,11 +6,13 @@ struct ComplexNumbers{
     int a, b ,c ,d;
 };
 
+//function to enter the complex number
 void enterComplexNumber(ComplexNumbers &number){
     cin >> number.a >> number.b;
     cin >> number.c >> number.d;
 }
 
+//function to add the complex numbers
 void addComplexNumbers(ComplexNumbers number){
     int real = number.a + number.c;
     int imaginary = number.b + number.d;
@@ -18,10 +20,9 @@ void addComplexNumbers(ComplexNumbers number){
     if (real == 0) cout << imaginary << "i" << endl;
     else if (imaginary == 0) cout << real << endl;
     else cout << real << " + " << imaginary << "i" << endl;
-    
-    
 }
 
+//function to subtract the complex numbers
 void subtractComplexNumbers(ComplexNumbers number){
     int real = number.a - number.c; 
     int imaginary = number.b - number.d;
@@ -29,9 +30,9 @@ void subtractComplexNumbers(ComplexNumbers number){
     if (real == 0) cout << imaginary << "i" << endl;
     else if (imaginary == 0) cout << real << endl;
     else cout << real << " + " << imaginary << "i" << endl;
-    
 }
 
+//function to multiply the complex numbers
 void multiplyComplexNumbers(ComplexNumbers number){
     int real = (number.a * number.c) - (number.b * number.d);
     int imaginary = (number.a * number.d) + (number.b * number.c);
@@ -39,7 +40,6 @@ void multiplyComplexNumbers(ComplexNumbers number){
     if (real == 0) cout << imaginary << "i" << endl;
     else if (imaginary == 0) cout << real << endl;
     else cout << real << " + " << imaginary << "i" << endl;
-    
 }
 
 
@@ -49,9 +49,7 @@ int main(){
     enterComplexNumber(number);
 
     addComplexNumbers(number);
-
     subtractComplexNumbers(number);
-
     multiplyComplexNumbers(number);
 
     return 0;
